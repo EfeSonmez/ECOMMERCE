4557e9c5244abb66deb571d21b411c65ce1e318d
EfeSonmez <h.efe_sonmez@hotmail.com>
2022-06-16 00:59:02 +0300

    ordering by -created

--- store/models.py
+++ store/models.py
@@ -28,7 +28,9 @@ class Product(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-
+class Meta:
+    verbose_name_plural = 'Products'
+    ordering = ('-created')
 
 
 
